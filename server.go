7d9092a7d8d6379420b866f565319774727b35e4
Jan-Christoph Meier <jan-christoph.meier@outlook.de>
2023-12-01 20:47:34 +0100

    Make it possible to set mongodb connection

--- server.go
+++ server.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	mongodbURI, exists := os.LookupEnv("MONGODB_CONNECTION_STRING")
+	if !exists {
+		panic("Env variable MONGODB_CONNECTION_STRING needs to be set")
+	}
 
-	repo := CreateRepository()
+	repo := CreateRepository(mongodbURI)
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello")
